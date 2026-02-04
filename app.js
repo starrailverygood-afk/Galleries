403e20a25389ebbb89a5674fdd861eff51ba61c8
starrailverygood-afk <starrailverygood@gmail.com>
2026-02-05 01:06:33 +0800

    自动更新: 2026-02-05 01:06:33

--- app.js
+++ app.js
@@ -151,6 +151,39 @@ const LOCAL_GALLERY_DATA = [
     "imageFiles": []
   }
 ];
+    "tags": [],
+    "fileCount": 272,
+    "imageFiles": []
+  },
+  {
+    "id": "gallery-002",
+    "name": "甘雨-1-DIY",
+    "folderPath": "甘雨-1-DIY",
+    "character": [
+      "甘雨"
+    ],
+    "tags": [
+      "1",
+      "DIY"
+    ],
+    "fileCount": 20,
+    "imageFiles": []
+  },
+  {
+    "id": "gallery-003",
+    "name": "甘雨-2-咬",
+    "folderPath": "甘雨-2-咬",
+    "character": [
+      "甘雨"
+    ],
+    "tags": [
+      "2",
+      "咬"
+    ],
+    "fileCount": 27,
+    "imageFiles": []
+  }
+];
     "tags": [
       "正常"
     ],
